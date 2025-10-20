0ae7d215eaeed9560a19ae2cf21805def64687d6
Schiwo <moritz.schiwo@gmail.com>
2025-10-20 14:44:20 +0000

    Deploying to gh-pages from @ Schiwo/schiwo.github.io@15a4897568119f8cc82ed7f20c07e87a72efb265 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -422,6 +422,13 @@ ninja.data = [{
           window.open("mailto:%6D.%73%63%68%69%6C%74%65%6E%77%6F%6C%66@%6C%6D%75.%64%65", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/moritz-schiltenwolf-07a068206", "_blank");
+        },
+      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
